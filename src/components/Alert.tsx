47ee4cfce96280802ea4bcd2474df7c84a801968
reloadercf <elcfm1@gmail.com>
2023-10-19 21:04:10 -0600

    style: :lipstick: Improve colors of UI Alert

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -18,24 +18,31 @@ const Alert: React.FC<AlertPops> = ({ message, kind, setNotification }) => {
         message: null,
         kind: null,
       });
-    }, 6000);
+    }, 8000);
   }, []);
 
   return (
     <div>
       {kind === 'success' ? (
         <div
-          className="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
+          className="bg-cyan-200 border-t-4 border-cyan-600 rounded-b text-cyan-700 px-4 py-3 shadow-md"
           role="alert"
         >
-          <div className="flex">
+          <div className="flex justify-center">
             <div className="py-1">
               <svg
-                className="fill-current h-6 w-6 text-teal-500 mr-4"
+                className="fill-current h-6 w-6 text-cyan-700 mr-4"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
               >
-                <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+                <path
+                  fill="currentColor"
+                  d="m18.774 8.245-.892-.893a1.5 1.5 0 0 1-.437-1.052V5.036a2.484 2.484 0 0 0-2.48-2.48H13.7a1.5 1.5 0 0 1-1.052-.438l-.893-.892a2.484 2.484 0 0 0-3.51 0l-.893.892a1.5 1.5 0 0 1-1.052.437H5.036a2.484 2.484 0 0 0-2.48 2.481V6.3a1.5 1.5 0 0 1-.438 1.052l-.892.893a2.484 2.484 0 0 0 0 3.51l.892.893a1.5 1.5 0 0 1 .437 1.052v1.264a2.484 2.484 0 0 0 2.481 2.481H6.3a1.5 1.5 0 0 1 1.052.437l.893.892a2.484 2.484 0 0 0 3.51 0l.893-.892a1.5 1.5 0 0 1 1.052-.437h1.264a2.484 2.484 0 0 0 2.481-2.48V13.7a1.5 1.5 0 0 1 .437-1.052l.892-.893a2.484 2.484 0 0 0 0-3.51Z"
+                />
+                <path
+                  fill="#fff"
+                  d="M8 13a1 1 0 0 1-.707-.293l-2-2a1 1 0 1 1 1.414-1.414l1.42 1.42 5.318-3.545a1 1 0 0 1 1.11 1.664l-6 4A1 1 0 0 1 8 13Z"
+                />
               </svg>
             </div>
             <div>
@@ -45,11 +52,23 @@ const Alert: React.FC<AlertPops> = ({ message, kind, setNotification }) => {
         </div>
       ) : (
         <div
-          className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+          className="bg-pink-300 border border-pink-500 text-pink-600 px-4 py-3 rounded relative flex justify-center"
           role="alert"
         >
-          <strong className="font-bold">Error!</strong>
-          <span className="block sm:inline">{message}</span>
+          <svg
+            className="fill-current h-6 w-6 text-pink-600 mr-4"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M7 3 6 2V1m5 2 1-1V1M9 7v11M9 7a5 5 0 0 1 5 5M9 7a5 5 0 0 0-5 5m5-5a4.959 4.959 0 0 1 2.973 1H12V6a3 3 0 0 0-6 0v2h.027A4.959 4.959 0 0 1 9 7Zm-5 5H1m3 0v2a5 5 0 0 0 10 0v-2m3 0h-3m-9.975 4H2a1 1 0 0 0-1 1v2m13-3h2.025a1 1 0 0 1 1 1v2M13 9h2.025a1 1 0 0 0 1-1V6m-11 3H3a1 1 0 0 1-1-1V6"
+            />
+          </svg>
+          <span className="block sm:inline"> {message}</span>
         </div>
       )}
     </div>
