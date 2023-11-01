9db7cb32f4e238f9a502cd67d66f15b9e660a3a6
reloadercf <elcfm1@gmail.com>
2023-10-31 22:18:27 -0600

    Create Provider for session

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,4 +1,7 @@
-import React, { ReactNode, createContext, useState } from 'react';
+import { useCookies } from 'react-cookie';
+import React, { ReactNode, createContext, useEffect, useState } from 'react';
+
+import connect from '../lib/connectAxios';
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -8,7 +11,26 @@ const AuthContext = createContext();
 
 const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [auth, setAuth] = useState({});
+  const [cookies] = useCookies(['session']);
 
+  useEffect(() => {
+    if (!cookies.session) return;
+    const config = {
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${cookies.session}`,
+      },
+    };
+    try {
+      const getProfile = async () => {
+        const { data } = await connect.user('/profile ', config);
+        setAuth(data);
+      };
+      getProfile();
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>
       {children}
